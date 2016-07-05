f619fdd0c6a5c8a095504104ba46a5f0922d8355
a-cordier <com.acordier@gmail.com>
2016-07-05 23:24:56 +0200

    install script'

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ SERVER_HOST=192.168.0.14
 set -e
 
 setup_build_tools(){
-	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 	sudo echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 	sudo apt-get update
