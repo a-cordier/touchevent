522dcf457b901d9a80113e495c97849eb5a13198
a-cordier <com.acordier@gmail.com>
2016-07-06 14:50:31 +0200

    install script'

--- install.sh
+++ install.sh
@@ -27,8 +27,9 @@ setup_node_js(){
 setup_mongo_db(){
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
     echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" \
-    	| sudo tee /etc/apt/sources.list.d/mongodb.list	sudo apt-get install -y mongodb-org
+    	| sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
+    sudo apt-get install -y mongodb-org
 	sudo cp ./disable-transparent-hugepages /etc/init.d
 	sudo chmod 755 /etc/init.d/disable-transparent-hugepages
 	sudo update-rc.d disable-transparent-hugepages defaults
