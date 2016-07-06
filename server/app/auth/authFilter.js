60a2b2cd08caa39ed44e2d403be535431ce26da2
a-cordier <com.acordier@gmail.com>
2016-07-06 19:47:23 +0200

    mouving token to cookies

--- server/app/auth/authFilter.js
+++ server/app/auth/authFilter.js
@@ -12,7 +12,7 @@ var Filter = function(router){
 
   // decode token
   if (token) {
-    logger.info('token: ' + req.cookies.token);
+    logger.info('token: ' + req.cookies.jwt);
     // verifies secret and checks exp
     jwt.verify(token, cfg.secret, function(err, decoded) {      
       if (err) {
