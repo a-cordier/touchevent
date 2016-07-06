3ca404e84b0a0640ccb0905002d4f3063d2e062b
a-cordier <com.acordier@gmail.com>
2016-07-06 22:40:33 +0200

    passport

--- server/app/controllers/authController.js
+++ server/app/controllers/authController.js
@@ -49,42 +49,7 @@ router.post('/',
 			success: true,
 			message: 'Token gen.'
 		});
-	});
-// router.post('/', function(req, res) {
-// 	User.findOne({
-// 		username: req.body.username
-// 	}, function(err, user) {
-// 		if (err) throw err;
+});
 
-// 		if (!user) {
-// 			logger.error(err);
-// 			res.status(401).send({
-// 				message: 'Authentication failed'
-// 			});
-// 		} else {
-// 			// Check if password matches
-// 			user.comparePassword(req.body.password, function(err, match) {
-// 				if (match && !err) {
-// 					// Create token if the password matched and no error was thrown
-// 					var token = jwt.sign(user, cfg.secret, {
-// 						expiresInMinutes: 1440 // 24h.
-// 					});
-// 					res.cookie('jwt', token, {
-// 						maxAge: 900000,
-// 						httpOnly: true
-// 					})
-// 					res.status(200).send({
-// 						success: true,
-// 						message: 'Token gen.'
-// 					});
-// 				} else {
-// 					res.status(401).send({
-// 						message: 'Authentication failed'
-// 					});
-// 				}
-// 			});
-// 		}
-// 	});
-// });
 
 module.exports = router;
