97fd8fe51fa9a8746c2417ae9df64b6f7e01b831
a-cordier <com.acordier@gmail.com>
2016-07-07 18:44:25 +0200

    passport middleware

--- server/app/controllers/stateController.js
+++ server/app/controllers/stateController.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var User = require('../model/user');
 var logger = require('../util/logger');
-var filter = require('../auth/authFilter');
+//var filter = require('../auth/authFilter');
 /* This controller basicaly just sends a ok
 if a granted token was provided on request */
-filter(router); // token auth
+// filter(router); // token auth
 router.post('/', function(req, res) {
 	res.status(200).json({});
 });
