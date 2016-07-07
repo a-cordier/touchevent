8de72ccc05874446f654d31e30d666089613ff56
a-cordier <com.acordier@gmail.com>
2016-07-07 11:33:49 +0200

    passport-jwt opts

--- server/app/auth/authFilter.js
+++ server/app/auth/authFilter.js
@@ -48,41 +48,41 @@ var Filter = function(req, res, next, callback) {
   })(req, res, next);
 }
 
-var Filter = function(router) {
-  router.use(function(req, res, next) {
+// var Filter = function(router) {
+//   router.use(function(req, res, next) {
 
-    // check header or url parameters or post parameters for token
-    var token = req.cookies.jwt;
-    // decode token
-    if (token) {
-      logger.info('token: ' + req.cookies.jwt);
-      // verifies secret and checks exp
-      jwt.verify(token, cfg.secret, function(err, decoded) {
-        if (err) {
-          return res.status(401).json({
-            success: false,
-            message: 'Failed to authenticate token.'
-          });
-        } else {
-          // if everything is good, save to request for use in other routes
-          req.decoded = decoded;
-          logger.info(JSON.stringify(decoded));
-          logger.info(decoded.role);
-          next();
-        }
-      });
+//     // check header or url parameters or post parameters for token
+//     var token = req.cookies.jwt;
+//     // decode token
+//     if (token) {
+//       logger.info('token: ' + req.cookies.jwt);
+//       // verifies secret and checks exp
+//       jwt.verify(token, cfg.secret, function(err, decoded) {
+//         if (err) {
+//           return res.status(401).json({
+//             success: false,
+//             message: 'Failed to authenticate token.'
+//           });
+//         } else {
+//           // if everything is good, save to request for use in other routes
+//           req.decoded = decoded;
+//           logger.info(JSON.stringify(decoded));
+//           logger.info(decoded.role);
+//           next();
+//         }
+//       });
 
-    } else {
+//     } else {
 
-      // if there is no token
-      // return an error
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
+//       // if there is no token
+//       // return an error
+//       return res.status(403).send({
+//         success: false,
+//         message: 'No token provided.'
+//       });
 
-    }
-  });
-}
+//     }
+//   });
+// }
 
 module.exports = Filter;
