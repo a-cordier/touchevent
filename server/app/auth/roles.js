997b591fe81a7aaafa537692603a488455fed727
a-cordier <com.acordier@gmail.com>
2016-07-09 14:39:02 +0200

    build

--- server/app/auth/roles.js
+++ server/app/auth/roles.js
@@ -2,29 +2,36 @@ var logger = require('../util/logger');
 
 var filter = function(role, req, res) {
 	if (!req.user)
-		return res.status(401).send({"message":"unauthorized"});
-	if (req.user.role !== role.id && role.includes.indexOf(req.user.role)<0)
-		return res.status(403).send({"mesage": "forbidden"});
+		return res.status(401).send({
+			"message": "unauthorized"
+		});
+	if (req.user.role !== role.id && role.includes.indexOf(req.user.role) < 0)
+		return res.status(403).send({
+			"mesage": "forbidden"
+		});
 	return next();
 };
 
+var Role = function(id, includes) {
+	this.id = id;
+	this.includes = includes || [];
+}
+
+Role.prototype.filter = function(req, res, next) {
+	if (!req.user)
+		return res.status(401).send({
+			"message": "unauthorized"
+		});
+	if (req.user.role !== this.id && this.includes.indexOf(req.user.role) < 0)
+		return res.status(403).send({
+			"mesage": "forbidden"
+		});
+	return next();
+}
 
 var Roles = {
-	admin: {
-		id: "admin",
-		includes: ["member"],
-		filter: function(req, res, next) {
-			logger.info(this.includes);
-			return filter(this, req, res, next);
-		}
-	},
-	member: {
-		id: "member",
-		includes: [],
-		filter: function(req, res, next) {
-			return filter(this, req, res, next);
-		}
-	}
+	admin: new Role("admin", ["member"]),
+	member: new Role("member")
 };
 
 module.exports = Roles;
