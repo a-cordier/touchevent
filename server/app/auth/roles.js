40343722358a983773a4e03f7198cb47c6e82f10
a-cordier <com.acordier@gmail.com>
2016-07-09 15:11:46 +0200

    build

--- server/app/auth/roles.js
+++ server/app/auth/roles.js
@@ -1,25 +1,23 @@
 var logger = require('../util/logger');
 
 var Role = function(id, includes) {
-	this.id = id;
-	this.includes = includes || [];
-	logger.info(this.id);
-	logger.info(this.includes);
+	var self = this;
+	self.id = id;
+	self.includes = includes || [];
+	self.filter = function(req, res, next) {
+		logger.info('my id: ' + self.id);
+		logger.info('my includes: ' + self.includes);
+		if (!req.user)
+			return res.status(401).send({
+				"message": "unauthorized"
+			});
+		if (req.user.role !== self.id && self.includes.indexOf(req.user.role) < 0)
+			return res.status(403).send({
+				"mesage": "forbidden"
+			});
+		return next();
+	};
 }
-
-Role.prototype.filter = function(req, res, next) {
-	logger.info('my id: ' + this.id);
-	logger.info('my includes: ' + this.includes);
-	if (!req.user)
-		return res.status(401).send({
-			"message": "unauthorized"
-		});
-	if (req.user.role !== this.id && this.includes.indexOf(req.user.role) < 0)
-		return res.status(403).send({
-			"mesage": "forbidden"
-		});
-	return next();
-};
 
 var Roles = {
 	admin: new Role("admin", ["member"]),
