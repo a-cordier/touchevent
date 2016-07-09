266e654d9069f60caf0f22c832e84778288fb822
a-cordier <com.acordier@gmail.com>
2016-07-09 15:45:20 +0200

    jwt-token ccallback

--- server/app/auth/roles.js
+++ server/app/auth/roles.js
@@ -1,25 +1,42 @@
 var logger = require('../util/logger');
 
+// var Role = function(id, includes) {
+// 	var self = this;
+// 	self.id = id;
+// 	self.includes = includes || [];
+// 	self.filter = function(req, res, next) {
+// 		if (!req.user)
+// 			return res.status(401).send({
+// 				"message": "unauthorized"
+// 			});
+// 		if (req.user.role !== self.id && self.includes.indexOf(req.user.role) < 0)
+// 			return res.status(403).send({
+// 				"mesage": "forbidden"
+// 			});
+// 		return next();
+// 	};
+// }
+
 var Role = function(id, includes) {
-	var self = this;
 	this.id = id;
 	this.includes = includes || [];
-	this.filter = function(req, res, next) {
-		if (!req.user)
-			return res.status(401).send({
-				"message": "unauthorized"
-			});
-		if (req.user.role !== this.id && this.includes.indexOf(req.user.role) < 0)
-			return res.status(403).send({
-				"mesage": "forbidden"
-			});
-		return next();
-	};
 }
+
+Role.prototype.filter = function(req, res, next) {
+	if (!req.user)
+		return res.status(401).send({
+			"message": "unauthorized"
+		});
+	if (req.user.role !== this.id && this.includes.indexOf(req.user.role) < 0)
+		return res.status(403).send({
+			"mesage": "forbidden"
+		});
+	return next();
+};
 
 var Roles = {
 	admin: new Role("admin"),
-	member: new Role("member", ["admin"])  // admin extends member
+	member: new Role("member", ["admin"]) // admin extends member
 };
 
 module.exports = Roles;
