2ac676e89614fa0afe9b8b5d3c7d57da1ff08e1d
a-cordier <com.acordier@gmail.com>
2016-07-09 16:20:14 +0200

    jwt-token ccallback

--- server/app/auth/roles.js
+++ server/app/auth/roles.js
@@ -1,39 +1,40 @@
 var logger = require('../util/logger');
 
 
-var Role = function(id, ext) {
-	var self = this;
-	self.id = id;
-	self.ext = ext || [];
-	self.filter = function(req, res, next) {
-		if (!req.user)
-			return res.status(401).send({
-				"message": "unauthorized"
-			});
-		if (req.user.role !== self.id && self.ext.indexOf(req.user.role) < 0)
-			return res.status(403).send({
-				"mesage": "forbidden"
-			});
-		return next();
-	};
-}
-
-// function Role(id, ext) {
-// 	this.id = id;
-// 	this.ext = ext || [];
+// var Role = function(id, ext) {
+// 	var self = this;
+// 	self.id = id;
+// 	self.ext = ext || [];
+// 	self.filter = function(req, res, next) {
+// 		if (!req.user)
+// 			return res.status(401).send({
+// 				"message": "unauthorized"
+// 			});
+// 		if (req.user.role !== self.id && self.ext.indexOf(req.user.role) < 0)
+// 			return res.status(403).send({
+// 				"mesage": "forbidden"
+// 			});
+// 		return next();
+// 	};
 // }
 
-// Role.prototype.filter = function(req, res, next) {
-// 	if (!req.user)
-// 		return res.status(401).send({
-// 			"message": "unauthorized"
-// 		});
-// 	if (req.user.role !== this.id && this.ext.indexOf(req.user.role) < 0)
-// 		return res.status(403).send({
-// 			"mesage": "forbidden"
-// 		});
-// 	return next();
-// };
+function Role(id, ext) {
+	this.id = id;
+	this.ext = ext || [];
+	this.filter=this.filter.bind(this);
+}
+
+Role.prototype.filter = function(req, res, next) {
+	if (!req.user)
+		return res.status(401).send({
+			"message": "unauthorized"
+		});
+	if (req.user.role !== this.id && this.ext.indexOf(req.user.role) < 0)
+		return res.status(403).send({
+			"mesage": "forbidden"
+		});
+	return next();
+};
 
 var Roles = {
 	admin: new Role("admin"),
