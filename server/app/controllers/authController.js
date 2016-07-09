e389a91a5ca05bef2bb223a033743aad8f6eb95a
a-cordier <com.acordier@gmail.com>
2016-07-09 10:54:26 +0200

    jwt-token ccallback

--- server/app/controllers/authController.js
+++ server/app/controllers/authController.js
@@ -59,7 +59,7 @@ router.post('/', function(req, res, next) {
 			if (err) {
 				return next(err);
 			}
-			var token = jwt.sign(user.toJson(), cfg.secret, {
+			var token = jwt.sign(user.toJSON(), cfg.secret, {
 				expiresInMinutes: 1440 // 24h.
 			});
 			res.cookie('jwt', token, {
