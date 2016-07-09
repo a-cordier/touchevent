98ccebd30c169bd3c54d9c1f42ba25b1aae25a63
a-cordier <com.acordier@gmail.com>
2016-07-09 14:24:51 +0200

    build

--- server/app/auth/roles.js
+++ server/app/auth/roles.js
@@ -14,7 +14,7 @@ var Roles = {
 		id: "admin",
 		includes: ["member"],
 		filter: function(req, res, next) {
-			logger.info(JSON.stringify(this));
+			logger.info(this.includes);
 			return filter(this, req, res, next);
 		}
 	},
