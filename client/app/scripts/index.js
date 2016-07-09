104ffa21307d5a1c6c80a8e773b3cbd0438c4717
a-cordier <com.acordier@gmail.com>
2016-07-09 11:28:50 +0200

    jwt-token ccallback

--- client/app/scripts/index.js
+++ client/app/scripts/index.js
@@ -20,7 +20,7 @@ define(["jquery", "backbone", "underscore", "./loader"],
 			cache: false,
 			statusCode: {
 				401: function(req, status, error) {
-					console.log(req.responseText.resource);
+					console.log(req);
 					Backbone.history.navigate('login', {trigger: true});
 				}
 			}
