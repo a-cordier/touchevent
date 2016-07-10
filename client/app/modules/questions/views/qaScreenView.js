d209c01f457cadae87be54653afd3c16a77ff78c
a-cordier <com.acordier@gmail.com>
2016-07-10 16:05:33 +0200

    socket.io models

--- client/app/modules/questions/views/qaScreenView.js
+++ client/app/modules/questions/views/qaScreenView.js
@@ -7,7 +7,7 @@ define(["backbone", "jquery", "commons/views/PageView", "commons/viewHolder",
   function(Backbone, $, PageView, ViewHolder,
     _, text, _template, 
     QaSynthCollection) {
-    var SynthView = PageView.extend({
+    return PageView.extend({
 
       name: "QaSynthView",
       
@@ -20,25 +20,29 @@ define(["backbone", "jquery", "commons/views/PageView", "commons/viewHolder",
       },
 
       initialize: function(options) {
+        this.qas = options.qas;
+        this.listenTo(this.qas, 'remove', this.remove);
+        this.listenTo(this.qas, 'add', this.add);
         _.bindAll(this, "render");
-        _.bindAll(this, "update");
+        _.bindAll(this, "add");
+        _.bindAll(this, "remove");
         this.delegateEvents(this.events);
         ViewHolder.registerView(this);
       },
 
-      sync: function(){},
-      
-      update: function(data) {
-        if(!data)
-          data = "";
-        $('#synth').html('<p>QUESTION: ' + data + '</p>');
+      add: function(model){
+        // TODO: transition ?
+        this.render();
+      },
+
+      remove: function(model){
+          $('#synth-content').remove();
       },
 
       render: function() {
-        PageView.prototype.render.apply(this, []);
+        PageView.prototype.render.apply(this, {qas: this.qas});
         $(this.el).addClass('synth-screen');
         return this;
       }
     });
-    return SynthView;
   });
